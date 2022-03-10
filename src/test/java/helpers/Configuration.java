80b733840d1bc1f5787582260e2d8ed02af7279a
Fabian Machicao <fabian.machicao@gmail.com>
2022-03-10 07:51:01 -0400

    Primer Commit

---
+++ src/test/java/helpers/Configuration.java
@@ -0,0 +1,14 @@
+package helpers;
+
+public class Configuration {
+    public static String host="https://todo.ly";
+    public static String user="fabian.machicao@gmail.com";
+    public static String pwd="12345";
+
+    //Nuevo Usuario
+    //Se deben actualizar los campos newUser y newUserEmail cada que se crea un usuario
+    public static String fullName="Andre Machicao";
+    public static String newUser="nuevo_usuario17@api.com";
+    public static String newEmail="New_nuevo_usuario17@api.com";
+    public static String newPwd="12345";
+}
